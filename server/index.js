0d4662f881ff7003469bef5bc4ec09e1e1e0fc49
Nix404 <sinofgreed693@gmail.com>
2026-04-16 12:07:20 +0530

    added routes

--- server/index.js
+++ server/index.js
@@ -1,14 +1,34 @@
 import express from "express";
+import { createServer, get, METHODS } from "http";
+import { Server } from "socket.io";
 
 const app = express();
-
-app.get('/', (_req, res) => {
-    res.status(201).json({
-        message: "success"
-    })
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
 })
 
-app.listen(3000, ()=> {
-    console.log(`Listening on port 3000`);
-    
-})
+app.get("/one-on-one", (_req, res) => {
+  res.status(201).json({
+    message: "success",
+  });
+});
+
+app.get("/grp-chat", (_req, res) => {
+  res.status(201).json({
+    message: "success",
+  });
+});
+
+app.get("/broadcast", (_req, res) => {
+  res.status(201).json({
+    message: "success",
+  });
+});
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
